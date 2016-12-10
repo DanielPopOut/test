59e98ae54e7ca1b3a936376941c39e21f9d8bf11
DanielPopOut <stephane.tchangang@gmail.com>
2016-12-10 21:36:20 +0100

    oubli virgule

--- js/endpoints/participant.js
+++ js/endpoints/participant.js
@@ -82,9 +82,9 @@ module.exports = function (app,bcrypt,dateFormat,ObjectId,db) {
 
 			participantdb.findAndModify({
 				query: {eventId: joinParticipant.eventId, guestId: joinParticipant.guestId},
-				update : {$set: {status:joinParticipant.status, modified: new Date()} }
-				upsert: true }
-				,function(err, result) {
+				update : {$set: {status:joinParticipant.status, modified: new Date()} },
+				upsert: true },
+				function(err, result) {
 					if(err) {
 						console.log('********************************');
 						console.log('Error while inserting ' + collectionName);
